4cb67b252ae002d2e996b19b2dcd430ba39beb45
Bibek Hanal <bbekhamal26@gmail.com>
2025-06-01 22:39:15 +1000

    updated card

--- src/card.jsx
+++ src/card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import profilePic from './assets/image.png'
 
 const card = () => {
   return (
     <div className='card'>
-        <img></img>
-        <h2>Bibek Hamal</h2>
-        <p>I am a MERN stack developer.</p>
+        <img className="card-image" alt='My picture' src={profilePic}></img>
+        <h2 className='card-title'>Bibek Hamal</h2>
+        <p className='card-text'> I am a MERN stack developer.</p>
       
     </div>
   )

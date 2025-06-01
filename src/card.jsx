51653013cc3ec76fb6ccda605be7f755c6eed72a
Bibek Hanal <bbekhamal26@gmail.com>
2025-06-01 21:40:06 +1000

    first commit setting up the app

---
+++ src/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='card'>
+        <img></img>
+        <h2>Bibek Hamal</h2>
+        <p>I am a MERN stack developer.</p>
+      
+    </div>
+  )
+}
+
+export default card
